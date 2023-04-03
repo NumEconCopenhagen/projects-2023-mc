8fbab74a50871f34cb319b0cc7100b2342ad69a0
fmb622 <fmb622@alumni.ku.dk>
2023-04-03 14:54:33 +0200

    commit

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,4 +1,4 @@
-#This is a copy of "HouseholdSpecializationModel.py"
+#This is an edited version of "HouseholdSpecializationModel.py"
 
 from types import SimpleNamespace
 
@@ -32,7 +32,6 @@ class HouseholdSpecializationModelClass:
         par.wF = 1.0
         par.wF_vec = np.linspace(0.8,1.2,5)
 
-
         # e. targets
         par.beta0_target = 0.4
         par.beta1_target = -0.1
@@ -56,18 +55,13 @@ class HouseholdSpecializationModelClass:
         C = par.wM*LM + par.wF*LF
 
         # b. home production
-        H = HM**(1-par.alpha)*HF**par.alpha
-
-        if par.sigma==0:
-            H = np.min(HM,HF)
-        elif par.sigma==1:
+        if par.sigma == 0:
+            H = np.argmin(HM, HF)
+        elif par.sigma == 1:
             H = HM**(1-par.alpha)*HF**par.alpha
-        else :
-            HM= np.fmax(HM,1e-07)
-            HF== np.fmax(HF,1e-07)
-            inside =(1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma)
-            inside=np.fmax(inside,1e-07)
-            H=(inside)**(par.sigma/(par.sigma-1))
+        else:
+            H = ((1-par.alpha)*HM**((par.sigma-1)/par.sigma)+par.alpha*HF**((par.sigma-1)/par.sigma))**(par.sigma/(par.sigma-1)) 
+
 
         # c. total consumption utility
         Q = C**par.omega*H**(1-par.omega)
@@ -112,6 +106,8 @@ class HouseholdSpecializationModelClass:
         opt.LF = LF[j]
         opt.HF = HF[j]
 
+        opt.HFHM = HF[j]/HM[j]
+
         # e. print
         if do_print:
             for k,v in opt.__dict__.items():
@@ -121,12 +117,59 @@ class HouseholdSpecializationModelClass:
 
     def solve(self,do_print=False):
         """ solve model continously """
+        par = self.par
+        sol = self.sol 
+        opt = SimpleNamespace()
+        
+        # Making guesses for each parameter:
+        #LM,HM,LF,HF
+        LM_g=7
+        HM_g=5
+        LF_g=7
+        HF_g=5
+        All_g=[LM_g,HM_g,LF_g,HF_g]
 
-        pass    
+        # Creating objective 
+        objective_function = lambda x: -self.calc_utility(x[0],x[1],x[2],x[3])
+
+        # Setting bounds
+        bounds=[(0,24),(0,24),(0,24),(0,24)]
+        
+        # Finding result element and extracting values from it
+        res = optimize.minimize(objective_function,All_g,method='Nelder-Mead',bounds=bounds) 
+        opt.LM = res.x[0]
+        opt.HM = res.x[1]
+        opt.LF = res.x[2]
+        opt.HF = res.x[3]
+
+        # e. print
+        if do_print:
+            for k,v in opt.__dict__.items():
+                print(f'{k} = {v:6.4f}')
+
+        return opt
+        pass  
+   
 
     def solve_wF_vec(self,discrete=False):
         """ solve model for vector of female wages """
 
+        par = self.par
+        sol = self.sol
+
+        wF_vec = np.linspace(0.8, 1.2, 5)
+
+        for i, wF in enumerate(wF_vec):
+             par.wF = wF
+             if discrete == True:
+                opt = self.solve_discrete()
+             else:
+                opt = self.solve()
+             sol.HM_vec[i] = opt.HM
+             sol.LM_vec[i] = opt.LM
+             sol.LF_vec[i] = opt.LF
+             sol.HF_vec[i] = opt.HF
+        return sol
         pass
 
 
@@ -136,12 +179,33 @@ class HouseholdSpecializationModelClass:
         par = self.par
         sol = self.sol
 
+        
+
         x = np.log(par.wF_vec)
         y = np.log(sol.HF_vec/sol.HM_vec)
         A = np.vstack([np.ones(x.size),x]).T
-        sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0]
-    
-    def estimate(self,alpha=None,sigma=None):
-        """ estimate alpha and sigma """
+        sol.beta0,sol.beta1 = np.linalg.lstsq(A,y,rcond=None)[0] 
 
+        
+        
+    
+    def estimate(self):
+        """ estimate alpha and sigma """
+        par = self.par
+        sol = self.sol
+        
+        def error(x):
+            par.alpha, par.sigma = x
+            self.solve_wF_vec()
+            self.run_regression()
+            fejl = (par.beta0_target-sol.beta0)**2  + (par.beta1_target-sol.beta1)**2
+            return fejl
+        
+        x0 = [0.5,0.5]
+        bounds = [(0.001,1.0),(0.001,1.0)]
+        solution = optimize.minimize(error, x0, method='Nelder-Mead', bounds=bounds)
+        sol.alpha = solution.x[0]
+        sol.sigma = solution.x[1]
+       
         pass
+        
