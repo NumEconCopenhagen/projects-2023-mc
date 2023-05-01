2133be1340123aedcd28bde30ac0a3691659d9be
fmb622 <fmb622@alumni.ku.dk>
2023-05-01 11:24:18 +0200

    commit

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,25 @@
 from scipy import optimize
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+def solve_for_ss(s,g,n,alpha,delta):
+    """ solve for the steady state level of capital
 
     Args:
-        c (float): costs
-        alpha (float): parameter
+        s (float): saving rate
+        g (float): technological growth rate
+        n (float): population growth rate
+        alpha (float): cobb-douglas parameter
+        delta (float): capital depreciation rate 
 
     Returns:
-        result (RootResults): the solution represented as a RootResults object.
+        result (RootResults): the solution represented as a RootResults object
 
     """ 
     
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
+    # a. define objective function
+    f = lambda k: k**alpha
+    obj_kss = lambda kss: kss - (s*f(kss) + (1-delta)*kss)/((1+g)*(1+n))
 
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
+    #. b. call root finder
+    result = optimize.root_scalar(obj_kss,bracket=[0.1,100],method='bisect')
     
     return result
